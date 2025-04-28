c93deb29dfde398f3f043a6d6bdbe764804a2c71
kouamebi123 <60981459+kouamebi123@users.noreply.github.com>
2025-04-28 10:05:04 +0200

    Correction du corse

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const corsOptions = {
     credentials: true
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+//app.options('*', cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
